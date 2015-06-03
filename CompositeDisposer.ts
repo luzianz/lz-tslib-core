d56d6606d108ed2d85fc9ad789f531f54e1d7434
Luzian Zagadinow <luzianz@outlook.com>
2015-06-03 13:29:47 -0400

    add CompositeDisposer

---
+++ CompositeDisposer.ts
@@ -0,0 +1,18 @@
+/// <reference path="node_modules/lz-tslib-interfaces/IDisposable" />
+
+class CompositeDisposer implements IDisposable {
+	private disposables: IDisposable[];
+
+	add(disposable: IDisposable) {
+		this.disposables.push(disposable);
+	}
+
+	dispose() {
+		this.disposables.forEach(function (disposable) {
+			disposable.dispose();
+		});
+		this.disposables.splice(0, this.disposables.length);
+	}
+}
+
+export = CompositeDisposer;
