2458b1045e1ae629f9ac01aef650cf613d8bad0c
Luzian Zagadinow <luzianz@outlook.com>
2015-06-03 21:06:42 -0400

    export added classes

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 export import ValueChangedEmitter = require('./ValueChangedEmitter');
 export import ValueContainer = require('./ValueContainer');
 export import EventEmitter = require('./EventEmitter');
+export import DelegateDisposer = require('./DelegateDisposer');
+export import CompositeDisposer = require('./CompositeDisposer');
+export import EndDerrivedSpan = require('./Spans/EndDerrivedSpan');
+export import LengthDerrivedSpan = require('./Spans/LengthDerrivedSpan');
+export import StartDerrivedSpan = require('./Spans/StartDerrivedSpan');
+export import TrimmableSpan = require('./Spans/TrimmableSpan');
+export import SpanExtensions = require('./Spans/Extensions');
+export import ArrayExtensions = require('./Array/Extensions');
