0f04e32a8cd02c1f41943f58e93b9616064d8f42
Luzian Zagadinow <luzianz@outlook.com>
2015-10-01 05:59:51 -0400

    add iterator extensions

--- Iterable/Extensions.ts
+++ Iterable/Extensions.ts
@@ -1,5 +1,5 @@
 /// <reference path="../node_modules/lz-tslib-interfaces/IIterator.d.ts" />
-/// <reference path="../node_modules/lz-tslib-interfaces/FFunc.d.ts" />
+/// <reference path="../node_modules/lz-tslib-interfaces/FPredicate.d.ts" />
 
 export function select<I,O>(iterator: IIterator<I>, selector: FFunc<I,O>): IIterator<O> {
 	return {
@@ -11,4 +11,41 @@ export function select<I,O>(iterator: IIterator<I>, selector: FFunc<I,O>): IIter
 			};
 		}
 	};
+}
+
+export function where<T>(iterator: IIterator<T>, predicate: FPredicate<T>): IIterator<T> {
+	return {
+		next: function() {
+			var result: {value?: T, done: boolean};
+			
+			while(true) {
+				result = iterator.next();
+				if (result.done || predicate(result.value)) {
+					break;
+				}
+			}
+			
+			return {
+				value: result.done? undefined : result.value,
+				done: result.done
+			};
+		}
+	};
+}
+
+export function first<T>(iterator: IIterator<T>, predicate: FPredicate<T>): T {
+	var item: T;
+	var result: {value?: T, done: boolean};
+	
+	while (true) {
+		result = iterator.next();
+		if (result.done) {
+			break;
+		} else if (predicate(result.value)) {
+			item = result.value;
+			break;
+		}
+	}
+	
+	return item;
 }
