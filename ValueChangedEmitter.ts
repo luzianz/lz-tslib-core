260f0a03a0d69b0fa5d73721cf2e74b20359a97f
Luzian Zagadinow <luzianz@outlook.com>
2015-10-01 05:19:30 -0400

    adapt to changed interface

--- ValueChangedEmitter.ts
+++ ValueChangedEmitter.ts
@@ -1,14 +1,24 @@
-/// <reference path="typings/lz-tslib-interfaces/index.d.ts" />
+/// <reference path="node_modules/lz-tslib-interfaces/FAction.d.ts" />
+/// <reference path="node_modules/lz-tslib-interfaces/INotifyPropertyChanged.d.ts" />
+/// <reference path="node_modules/lz-tslib-interfaces/INotifyPropertyChanging.d.ts" />
 
 import EventEmitter = require('./EventEmitter');
 
-class ValueChangedEmitter<T> extends EventEmitter implements INotifyValueChanged<T> {
-	protected onValueChanged(oldValue: T, newValue: T) {
-		this.emit('valueChanged', arguments);
+class ValueChangedEmitter<T> extends EventEmitter implements INotifyPropertyChanged, INotifyPropertyChanging {
+	protected onValueChanged() {
+		this.onPropertyChanged('value');
 	}
 	
 	protected onValueChanging(oldValue: T, newValue: T, cancel?: FAction) {
-		this.emit('valueChanging', arguments);
+		this.onPropertyChanging('value', oldValue, newValue, cancel);
+	}
+	
+	private onPropertyChanged(propertyName: string) {
+		this.emit('propertyChanged', arguments);
+	}
+	
+	private onPropertyChanging(propertyName: string, oldValue: T, newValue: T, cancel?: FAction) {
+		this.emit('propertyChanging', arguments);
 	}
 }
 
